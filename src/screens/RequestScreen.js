024d88e364be835d20aef2d10a8c69ade17c6f3d
JoseLuisHcoder <zonajl@gmail.com>
2022-10-18 20:24:12 -0400

    upload

--- src/screens/RequestScreen.js
+++ src/screens/RequestScreen.js
@@ -1,21 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import HomeScreen from './src/screens/HomeScreen'
 
-
-
-const App = () => {
+const RequestScreen = () => {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <Text>Request Screen</Text>
     </View>
   )
 }
 
-export default App
+export default RequestScreen
 
 const styles = StyleSheet.create({
   container:{
-    flex:1
+    paddingTop:20
   }
 })
